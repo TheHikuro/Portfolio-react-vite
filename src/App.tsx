61e39912acaa21e3fbaf83fd7cf11142f0884430
Loan CLERIS <loan.cleris@gmail.com>
2022-02-25 17:44:34 +0100

    Add React-Dom-Router
    Add last version of react-dom-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
 import HomePage from './pages/home-page/HomePage'
 import './App.css'
 import { initInternationalization } from './i18next'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 
 function App() {
 
   initInternationalization('fr')
 
   return (
-    <div className="App">
-      <HomePage />
+    <div className='App'>
+      <Router>
+        <Routes>
+          Route: <Route path='/' element={<HomePage />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,11 +1,15 @@
 import { useTranslation } from "react-i18next";
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+    const navigate = useNavigate()
     const { t } = useTranslation();
     return (
-        <div className='h-screen w-full flex justify-center'>
-            <h1>{t('default.name')}</h1>
-        </div>
+        <>
+            <div className='h-screen w-full flex justify-center'>
+                <h1>{t('default.name')}</h1>
+            </div>
+        </>
     );
 }
 
