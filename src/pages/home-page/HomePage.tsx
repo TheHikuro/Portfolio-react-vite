1b941bfed4540e04b215c068c61f3f6b22bd647d
Loan CLERIS <loan.cleris@gmail.com>
2022-02-25 16:12:27 +0100

    Init portfilio
    Framework :
    - ViteJS
    - ReactJS (TS)
    - Tailwind

---
+++ src/pages/home-page/HomePage.tsx
@@ -0,0 +1,10 @@
+
+const HomePage = () => {
+    return (
+        <div className='h-screen w-full flex justify-center'>
+            <h1>Home Page</h1>
+        </div>
+    );
+}
+
+export default HomePage;
